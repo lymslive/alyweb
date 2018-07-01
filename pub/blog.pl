0722176f73a1b13bed2fdf41c8ddd28795898a26
lymslive <403708621@qq.com>
2018-07-01 23:18:21 +0800

    new homepage

---
+++ pub/blog.pl
@@ -0,0 +1,15 @@
+#! /usr/bin/env perl
+package blog;
+use strict;
+use warnings;
+
+print "Content-type:text/html\n\n";
+print <<EndOfHTML;
+<html><head><title>Perl Environment Variables</title></head>
+<meta charset="utf-8">
+<body>
+<h1>七阶子博客</h1>
+敬请期待 todo ...
+EndOfHTML
+
+print "</body></html>";
