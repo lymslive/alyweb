2b1b3446856b4c6ea5774a500a7c2112fba3174b
lymslive <403708621@qq.com>
2019-02-22 00:01:11 +0800

    log

---
+++ family/WebLog.pm
@@ -0,0 +1,57 @@
+#! /usr/bin/env perl
+package WebLog;
+
+use Exporter 'import';
+@EXPORT = qw(wlog);
+
+use strict;
+use warnings;
+
+our @in_buff = ();
+our $to_std = 0;
+our $to_file = 0;
+our $disable = 0;
+
+sub init
+{
+	@in_buff = ();
+}
+
+sub wlog(@)
+{
+	return 1 if $disable;
+
+	my $msg = join(" ", @_);
+	my ($package, $filename, $line, $subroutine) = caller(1);
+	my $logstr = "[$filename:$line] ($subroutine) | $msg";
+
+	if ($to_std) {
+		print STDERR "$logstr\n";
+	}
+
+	push @in_buff, $logstr;
+	1; # always true
+}
+
+sub buff_to_std
+{
+	my ($var) = @_;
+	foreach my $logstr (@in_buff) {
+		print STDERR "$logstr\n";
+	}
+	
+}
+
+sub buff_to_str
+{
+	my ($br) = @_;
+	$br //= '';
+	return join($br, @in_buff);
+}
+
+sub buff_as_web
+{
+	return buff_to_str("<br>\n");
+}
+
+1;
